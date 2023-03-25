324e157272ac2e9d7a5df3b08d9d6d7ffae9ae02
Nmoek <707204150@qq.com>
2023-03-26 02:34:38 +0800

    判断替换为函数

--- src/hello_go/hello2_test.go
+++ src/hello_go/hello2_test.go
@@ -8,24 +8,30 @@ const test_str = "hello "
 
 func TestHello(t *testing.T) {
 
-	/*增加断言assert*/
+	/*增加断言assert函数 替代原来的判断*/
+	assertCorrectMsg := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got='%q' want='%q'", got, want)
+		}
+	}
 
+	/*分组测试1*/
 	t.Run("say hello to all peolpe", func(t *testing.T) {
 		got := Hello("ljk1")
-		want := "hello ljk1"
+		want := "hello ljk"
 
-		if got != want {
-			t.Errorf("test1 got='%q' want='%q' \n", got, want)
-		}
+		assertCorrectMsg(t, got, want)
+
 	})
 
+	/*分组测试2*/
 	t.Run("say hello when an empty string is supplied", func(t *testing.T) {
 		got := Hello("")
 		want := "hello world"
 
-		if got != want {
-			t.Errorf("test2 got='%q' want='%q' \n", got, want)
-		}
+		assertCorrectMsg(t, got, want)
+
 	})
 
 }
