66f10ada45938591ff7c8e64296ca4344aa8d790
Nmoek <707204150@qq.com>
2023-03-26 05:01:25 +0800

    切片求和后合并

--- src/variable/sum_test.go
+++ src/variable/sum_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"reflect" //对标深拷贝-->深比较, 不做类型检查单纯字节比较
 	"testing"
 )
 
@@ -17,18 +18,28 @@ func TestSum(t *testing.T) {
 		}
 
 	})
+}
 
-	t.Run("this is any numbers", func(t *testing.T) {
-		numbers := []int{1, 2, 3, 4, 5, 6, 7}
+func TestSumAll(t *testing.T) {
 
-		got := Sum(numbers)
-		want := 28
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
 
-		if got != want {
-			t.Errorf("got=%d want=%d given=%v", got, want, numbers)
-		}
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got=%v want=%v", got, want)
+	}
+}
 
-	})
+// 计算每一个切片的和，把所有切片的和一次放到一个新的切片中返回
+func SumAll(slices ...[]int) (sums []int) {
+	slice_nums := len(slices)
+	sums = make([]int, slice_nums) // 构建一个新的切片
+
+	for i, slice := range slices {
+		sums[i] = Sum(slice)
+	}
+
+	return
 }
 
 func Sum(numbers []int) int {
