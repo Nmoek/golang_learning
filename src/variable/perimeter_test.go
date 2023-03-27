ecb6a46aa7d1c29470aebb5f277b45eb6ec6ccf6
Nmoek <707204150@qq.com>
2023-03-28 01:49:58 +0800

    '方法'的引入与使用

--- src/variable/perimeter_test.go
+++ src/variable/perimeter_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"math"
 	"testing"
 )
 
@@ -13,17 +14,57 @@ type Rectangle struct {
 }
 
 /*
- * @breif: 测试返回长方形面积
+ * @brief: 长方形面积方法
+ */
+func (r Rectangle) Area() float64 {
+	return r.width * r.length
+}
+
+/*
+ * @brief: 圆形结构体定义
+ */
+type Circles struct {
+	radius float64
+}
+
+/*
+ * @brief: 圆形面积方法
+ */
+func (c Circles) Area() float64 {
+	return c.radius * c.radius * math.Pi
+}
+
+/*
+ * @breif: 测试返回长方形/圆形面积
  */
 func TestArea(t *testing.T) {
-	rect := Rectangle{3.0, 4.0}
-	got := Area(rect)
-	want := 12.0
 
-	if got != want {
-		t.Errorf("got=%.2f want=%.2f \n", got, want)
-	}
+	/* 此处解决重命名的方案有两个
+	 * 1.在新的包中重新定Aera(新参数)
+	 * 2. 定义"方法"。方法不等同于函数，函数可以任意时刻任意地方调用
+	 * ;方法必须依赖于一个对象进行调用
+	 */
+	t.Run("return Rectanle Area", func(t *testing.T) {
+		rect := Rectangle{3.0, 4.0}
+		// got := Area(rect)  //重命名点1
+		got := rect.Area()
+		want := 12.0
 
+		if got != want {
+			t.Errorf("got=%.2f want=%.2f \n", got, want)
+		}
+	})
+
+	t.Run("return Circles Area", func(t *testing.T) {
+		cir := Circles{1.0}
+		// got := Area(cir) //重命名点2
+		got := cir.Area()
+		want := math.Pi
+
+		if got != want {
+			t.Errorf("got=%.2f want=%.2f \n", got, want)
+		}
+	})
 }
 
 /*
