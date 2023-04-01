25f1eae7ffc728ccf0728f589e069613fd8b5071
Nmoek <707204150@qq.com>
2023-04-01 19:12:41 +0800

    将错误对象从函数中解耦,独立成全局变量

--- src/variable/pointer_test.go
+++ src/variable/pointer_test.go
@@ -28,6 +28,9 @@ func (w *Wallet) add(m Bitcoin) {
 	w.money += m
 }
 
+// 包内全局错误对象，针对处理钱包错误信息
+var InWalletErr = errors.New("cannot withdraw, insufficient funds")
+
 // @func: withDraw
 // @brief: 从钱包中取钱
 // @author: Kewin Li
@@ -36,7 +39,7 @@ func (w *Wallet) add(m Bitcoin) {
 func (w *Wallet) withDraw(m Bitcoin) error {
 
 	if m > w.money {
-		return errors.New("money not enough!!\n")
+		return InWalletErr
 	}
 
 	w.money -= m
@@ -62,12 +65,17 @@ func TestWallet(t *testing.T) {
 		}
 	}
 
-	assertErr := func(t *testing.T, err error) {
-		t.Helper()
+	assertErr := func(t *testing.T, got error, want error) {
 
-		if err == nil {
-			t.Errorf("want an error but not get \n")
+		if got == nil {
+			t.Fatal("want an error but not get \n")
 		}
+
+		// 获取到错误信息字符串
+		if got != want {
+			t.Errorf("got= %s want= %s\n", got, want)
+		}
+
 	}
 
 	t.Run("存钱", func(t *testing.T) {
@@ -87,13 +95,14 @@ func TestWallet(t *testing.T) {
 
 	})
 
+	// 引入error使用
 	t.Run("超额透支取钱", func(t *testing.T) {
 		w := Wallet{Bitcoin(20)}
 
-		err := w.withDraw(Bitcoin(100)) //只有10元却要取出30元
+		err := w.withDraw(Bitcoin(100)) // 只有20元却要取出100元 取出必定失败
 
 		errorMsg(t, w, Bitcoin(20))
-		assertErr(t, err)
+		assertErr(t, err, InWalletErr)
 
 	})
 
