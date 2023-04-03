6947d97d94e389ab02584eec199132f6a3978c88
Nmoek <707204150@qq.com>
2023-04-04 02:11:08 +0800

    接口不变，将实际睡眠动作--->计数动作

--- src/advanced/mocking_test.go
+++ src/advanced/mocking_test.go
@@ -13,16 +13,32 @@ import (
 	"io"
 	"os"
 	"testing"
-	"time"
 )
 
-func Count(out io.Writer, n int) {
-	for i := 3; i > 0; i-- {
-		time.Sleep(1 * time.Second)
+// Sleeper @brief: 自定义睡眠接口
+type Sleeper interface {
+	Sleep()
+}
+
+type SpySleep struct {
+	Calls int
+}
+
+// @func: Sleep
+// @brief: 模拟睡眠(实际没有睡眠，而是将睡眠动作转换为计数)
+// @author: Kewin Li
+// @receiver: *SpySleep s
+func (s *SpySleep) Sleep() {
+	s.Calls++
+}
+
+func Count(out io.Writer, n int, s *SpySleep) {
+	for i := n; i > 0; i-- {
+		s.Sleep()
 		fmt.Fprintln(out, i)
 	}
 
-	time.Sleep(1 * time.Second)
+	s.Sleep()
 	fmt.Fprintln(out, "GO!")
 }
 
@@ -30,7 +46,8 @@ func TestCount(t *testing.T) {
 
 	// 输出到标准输出
 	t.Run("out stdout", func(t *testing.T) {
-		Count(os.Stdout, 3)
+		spy := &SpySleep{}
+		Count(os.Stdout, 3, spy)
 	})
 
 	//输出到指定buffer
@@ -38,7 +55,9 @@ func TestCount(t *testing.T) {
 
 		buffer := bytes.Buffer{}
 
-		Count(&buffer, 3)
+		spy := &SpySleep{}
+
+		Count(&buffer, 3, spy)
 
 		got := buffer.String()
 		want := `3
@@ -48,6 +67,10 @@ GO!
 `
 		if got != want {
 			t.Errorf("got= '%s' want='%s' \n", got, want)
+		}
+
+		if spy.Calls != 4 {
+			t.Errorf("not enough wait seconds!\n")
 		}
 
 	})
