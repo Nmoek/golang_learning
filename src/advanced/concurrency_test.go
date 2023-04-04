e4d4ebd146f742b5176e0ce7b11ec9dedc4ca420
Nmoek <707204150@qq.com>
2023-04-04 23:04:35 +0800

    修改CheckWebsites函数位置

--- src/advanced/concurrency_test.go
+++ src/advanced/concurrency_test.go
@@ -9,7 +9,26 @@ package concurrency_test
 import (
 	"reflect"
 	"testing"
+	"time"
 )
+
+type WebsiteChecker func(string) bool
+
+// @func: CheckWebsites
+// @brief: 检查每一个传入的url合法性
+// @author: Kewin Li
+// @param: WebsiteChecker wc
+// @param: []string urls
+// @return map
+func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
+	results := make(map[string]bool)
+
+	for _, url := range urls {
+		results[url] = wc(url)
+	}
+
+	return results
+}
 
 // @func: mockWebsiteCHecker
 // @brief:
@@ -22,6 +41,32 @@ func mockWebsiteChecker(url string) bool {
 	}
 
 	return false
+}
+
+// @func: slowWebsiteChecker
+// @brief: 模拟查询url的耗时
+// @author: Kewin Li
+// @param: string _
+// @return bool
+func slowWebsiteChecker(_ string) bool {
+	time.Sleep(20 * time.Millisecond)
+	return true
+}
+
+// @func: BenchmarkCheckWebsites
+// @brief: 基准测试url合法查询, 性能粗侧
+// @author: Kewin Li
+// @param: *testing.B b
+func BenchmarkCheckWebsites(b *testing.B) {
+	urls := make([]string, 100)
+
+	for i := 0; i < 100; i++ {
+		urls[i] = "a url"
+	}
+
+	for i := 0; i < b.N; i++ {
+		CheckWebsites(slowWebsiteChecker, urls)
+	}
 }
 
 // @func: TestWebsiteChecker
