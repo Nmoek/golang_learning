389cbfc9015d32480777ef7155b62e8c9044db53
Nmoek <707204150@qq.com>
2023-04-05 17:31:16 +0800

    使用多线程完成http访问动作

--- src/advanced/select.go
+++ src/advanced/select.go
@@ -1,3 +1,10 @@
+/*
+ * @file: select.go
+ * @brief: select自测示例
+ * @author: Kewin Li
+ * @date: 2023-04-05
+ */
+
 package main
 
 import (
@@ -6,21 +13,49 @@ import (
 	"time"
 )
 
+type _result struct {
+	url      string
+	duration float64
+}
+
+// @func: GetDuration
+// @brief: 获取http请求返回时间间隔
+// @author: Kewin Li
+// @param: string url
+// @return float64
+func GetDuration(url string) float64 {
+
+	start := time.Now()
+	http.Get(url)
+
+	return float64(time.Since(start))
+
+}
+
 func main() {
 
 	a := "http://github.com"
 	b := "http://baidu.com"
 
-	start1 := time.Now()
-	http.Get(a)
-	d1 := time.Since(start1)
+	urls := []string{a, b}
+	channelResult := make(chan _result)
 
-	fmt.Printf("a url req duration = %.2f ms\n", float64(d1/1000000.0))
+	for _, url := range urls {
 
-	start2 := time.Now()
-	http.Get(b)
-	d2 := time.Since(start2)
+		go func(u string) {
+			channelResult <- _result{u, GetDuration(u)}
+		}(url)
 
-	fmt.Printf("a url req duration = %.2f ms\n", float64(d2/1000000.0))
+	}
 
+	for i := 0; i < len(urls); i++ {
+
+		go func() {
+			r := <-channelResult //疑似阻塞等待
+
+			fmt.Printf("url=%s duration=%.2f ms \n", r.url, r.duration/1000000.0)
+		}()
+	}
+
+	time.Sleep(11 * time.Second)
 }
