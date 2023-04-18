583cf86179577c1da6b114dad3d7d1019f02fe9b
Nmoek <707204150@qq.com>
2023-04-19 00:44:32 +0800

    [CHG] 不定参数传递

--- src/heima_shipin/06_不定个数参数列表.go
+++ src/heima_shipin/06_不定个数参数列表.go
@@ -14,6 +14,28 @@ func func1(args ...int) {
 	fmt.Printf("\n")
 }
 
+func func2_do(tmp ...int) {
+
+	fmt.Printf("tmp len=%d \n", len(tmp))
+
+	for _, val := range tmp {
+
+		fmt.Printf("%d ", val)
+	}
+
+	fmt.Printf("\n")
+
+}
+
+func func2(args ...int) {
+
+	func2_do(args...) //将本次不定参数列表全部传递
+
+	func2_do(args[:2]...) //切片形式部分传递 第一个元素~1
+
+	func2_do(args[2:]...) //切片形式部分传递 2~最后一个元素
+}
+
 func main() {
 
 	func1(1)
@@ -23,5 +45,7 @@ func main() {
 	func1(1, 2, 3)
 
 	fmt.Printf("---------------------- \n")
+
+	func2(1, 2, 3, 4, 5, 6)
 
 }
