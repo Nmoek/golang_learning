abb85d88cef1fe448881bbc25c81876d440e3adc
Nmoek <707204150@qq.com>
2023-04-21 00:45:32 +0800

    [CHG] 伪随机+随机数范围限定

--- src/heima_shipin/11_随机数产生.go
+++ src/heima_shipin/11_随机数产生.go
@@ -3,14 +3,18 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"time"
 )
 
 func main() {
 
-	rand.Seed(666)
+	// 每次设置一个不同的时间种子
+	// 注意: 伪随机，会出现重复的数
+	rand.Seed(time.Now().UnixNano())
 
 	for i := 0; i < 5; i++ {
-		fmt.Printf("rand= %d \n", rand.Int())
+		// 限定在100内的随机数
+		fmt.Printf("rand= %d \n", rand.Intn(100))
 	}
 
 }
