317ee5db39d1527f88265bb08f90d2fa039611b9
Nmoek <707204150@qq.com>
2023-05-13 18:13:50 +0800

    copy函数的基本用法

--- src/heima_shipin/14_切片的操作.go
+++ src/heima_shipin/14_切片的操作.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 // @func: test1
-// @brief: append函数基本用法
+// @brief: append函数基本用法以及2倍扩容策略
 // @author: Kewin Li
 func test1() {
 
@@ -18,38 +19,51 @@ func test1() {
 	s1 = append(s1, 3)
 	fmt.Printf("len=%d, cap=%d \n", len(s1), cap(s1))
 	// output: len=3, cap=4  至少这里可以看出扩容是2倍策略
+
+	fmt.Printf("----------------------------- \n")
+	fmt.Printf("append扩容变化: \n")
+	s2 := make([]int, 0, 1)
+	oldCap := cap(s2)
+	newCap := 0
+	for i := 0; i < 10; i++ {
+		s2 = append(s2, 1)
+		newCap = cap(s2)
+		if oldCap < newCap {
+			fmt.Printf("%d -----> %d \n", oldCap, newCap)
+			oldCap = newCap
+		}
+
+	}
+
 }
 
 // @func: test2
-// @brief: append函数的2倍扩容策略
+// @brief:  copy函数基本用法
 // @author: Kewin Li
 func test2() {
 
-	s1 := make([]int, 0, 1)
+	// copy函数的特点只会对应相当长度的空间进行值赋值
 
-	fmt.Printf("len=%d, cap=%d \n", len(s1), cap(s1))
-	//output: old len=0, cap=1
+	s1 := []int{1, 2}
+	s2 := []int{6, 6, 6, 6, 6}
 
-	s1 = append(s1, 1)
-	s1 = append(s1, 1)
+	fmt.Printf("s1=%v, p=%p\ns2=%v, p=%p\n", s1, &s1, s2, &s2)
 
-	fmt.Printf("len=%d, cap=%d \n", len(s1), cap(s1))
-	//output: len=2, cap=2
+	copy(s2, s1)
 
-	s1 = append(s1, 2)
-	s1 = append(s1, 2)
-	fmt.Printf("len=%d, cap=%d \n", len(s1), cap(s1))
-	//output: len=4, cap=4
+	fmt.Printf("s1=%v, p=%p\ns2=%v, p=%p\n", s1, &s1, s2, &s2)
 
-	s1 = append(s1, 3)
-	s1 = append(s1, 3)
-	fmt.Printf("len=%d, cap=%d \n", len(s1), cap(s1))
-	//output: len=6, cap=8
 }
 
 func main() {
 
-	// test1()
+	args := os.Args
 
-	test2()
+	switch args[1][0] {
+	case '1':
+		test1()
+	case '2':
+		test2()
+	}
+
 }
