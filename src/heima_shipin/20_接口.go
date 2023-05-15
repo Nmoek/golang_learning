4b7a93ef9f99582629eabf802a1aa269226b6844
Nmoek <707204150@qq.com>
2023-05-15 23:26:28 +0800

    [CHG] 接口继承以及转换；空接口的作用

--- src/heima_shipin/20_接口.go
+++ src/heima_shipin/20_接口.go
@@ -10,6 +10,35 @@ type Humaner interface {
 	sayHi()
 }
 
+// Personer @brief: 个人接口(interface)
+type Personer interface {
+	Humaner
+	singe(geming string)
+}
+
+// rapper @brief: rapper歌手结构体
+type rapper struct {
+	name    string
+	chanPai string // 厂牌
+}
+
+// @func: sayHi
+// @brief: repper结构体实现sayHi接口
+// @author: Kewin Li
+// @receiver: rapper r
+func (r rapper) sayHi() {
+	fmt.Printf("rapper info: %+v \n", r)
+}
+
+// @func: singe
+// @brief: repper结构体实现singe接口
+// @author: Kewin Li
+// @receiver: rapper r
+// @param: string geming
+func (r rapper) singe(geming string) {
+	fmt.Printf("i'm %s , is singing %s \n", r.name, geming)
+}
+
 type student struct {
 	name string
 	age  int
@@ -52,6 +81,46 @@ func (str myStr) sayHi() {
 // @param: Humaner h
 func whoSayHi(h Humaner) {
 	h.sayHi()
+}
+
+// @func: whoSinge
+// @brief: 嵌套多态接口，涉及接口转换(类似C++的向上转型)
+// @author: Kewin Li
+// @param: Personer p
+// @param: string geming
+func whoSinge(p Personer, geming string) {
+	whoSayHi(p)
+	if len(geming) > 0 {
+		p.singe(geming)
+	}
+}
+
+// @func: test3
+// @brief: 空接口类型
+// @author: Kewin Li
+func test3() {
+
+	// 1. 可以接收任意类型
+	var i interface{} = 1
+	fmt.Printf("i=%v \n", i)
+
+	i = "abc"
+	fmt.Printf("i=%v \n", i)
+
+	i = 3.212
+	fmt.Printf("i=%v \n", i)
+
+	// 2. 作可变参数列表的基础
+}
+
+// @func: test2
+// @brief: 接口继承、接口转换
+// @author: Kewin Li
+func test2() {
+
+	r := rapper{"jyx", "cbc"}
+	whoSinge(r, "真没睡")
+
 }
 
 // @func: test1
@@ -106,7 +175,10 @@ func main() {
 	switch args[1][0] {
 	case '1':
 		test1()
-
+	case '2':
+		test2()
+	case '3':
+		test3()
 	}
 
 }
