d09b7a159f36d9f68ef6b9e7b8de33bf302e8286
Nmoek <707204150@qq.com>
2023-07-21 21:34:04 +0800

    [CHG] 数组/切片深度理解

--- src/heima_shipin/10_数组.go
+++ src/heima_shipin/10_数组.go
@@ -15,6 +15,107 @@ func test1(nums [10]int) {
 	nums[0] = 666
 }
 
+// @func: myFindMax(helper function)
+// @brief: 找出数组中最大值的下标以及数值
+// @date: 2023年7月17日
+// @author: Kewin Li
+// @param: [10]int nums
+// @return idx
+// @return valMax
+func myFindMax(nums [10]int) (idx int, valMax int) {
+
+	valMax = -1
+
+	for i, v := range nums {
+		if v > valMax {
+			idx = i
+			valMax = v
+		}
+	}
+
+	return idx, valMax
+}
+
+// @func: numbers7
+// @brief: 多维数组
+// @date: 2023年7月17日
+// @author: Kewin Li
+func numbers7() {
+
+	rand.Seed(time.Now().UnixMicro())
+
+	var nums2 [2][2]int
+	for i := 0; i < 2; i++ {
+		for j := 0; j < 2; j++ {
+			nums2[i][j] = rand.Intn(100)
+		}
+	}
+
+	fmt.Printf("nums2=%v, addr0=%p, addr1=%p, len=%d, cap=%d\n", nums2, &nums2[0], &nums2[1], len(nums2), cap(nums2))
+
+	fmt.Printf("#####################\n")
+
+	// 1. 外层数组切片引用
+	slice1 := nums2[1:]
+
+	fmt.Printf("before slice1=%v, addr=%p, len=%d, cap=%d\n", slice1, &slice1, len(slice1), cap(slice1))
+
+	tmp := [2]int{666}
+	slice1 = append(slice1, tmp)
+	fmt.Printf("nums2=%v, addr0=%p, addr1=%p, len=%d, cap=%d\n", nums2, &nums2[0], &nums2[1], len(nums2), cap(nums2))
+
+	fmt.Printf("after slice1=%v, addr=%p, len=%d, cap=%d\n", slice1, &slice1, len(slice1), cap(slice1))
+
+	fmt.Printf("--------------------------\n")
+
+	// 2. 内层数组切片引用
+	slice2 := nums2[1][1:]
+
+	fmt.Printf("before slice2=%v, addr=%p, len=%d, cap=%d\n", slice2, &slice2, len(slice2), cap(slice2))
+
+	slice2 = append(slice2, 999)
+	fmt.Printf("nums2=%v, addr0=%p, addr1=%p, len=%d, cap=%d\n", nums2, &nums2[0], &nums2[1], len(nums2), cap(nums2))
+
+	fmt.Printf("after slice2=%v, addr=%p, len=%d, cap=%d\n", slice2, &slice2, len(slice2), cap(slice2))
+
+}
+
+// @func: numbers6
+// @brief: 26字母遍历
+// @date: 2023年7月17日
+// @author: Kewin Li
+func numbers6() {
+
+	zimu := [26]byte{0: 'A'}
+
+	for i := 0; i < 26; i++ {
+		zimu[i] = byte('A' + i)
+	}
+
+	for _, v := range zimu {
+		fmt.Printf("%c ", v)
+	}
+
+	fmt.Printf("\n")
+
+}
+
+func numbers5() {
+
+	var nums [5]float64
+	fmt.Printf("input float:\n")
+	for i := 0; i < len(nums); i++ {
+		fmt.Scanf("%f", &nums[i])
+	}
+
+	for i := 0; i < len(nums); i++ {
+		fmt.Printf("%.2f ", nums[i])
+	}
+
+	fmt.Printf("\n")
+
+}
+
 func numbers4() {
 
 	rand.Seed(time.Now().UnixMicro())
@@ -56,14 +157,18 @@ func numbers2() {
 	a := [2][2]int{{1, 2}, {3, 4}}
 	b := a[0]
 	pa := &a
-	pb := &a[1]
+	pa0 := &a[0]
+	pa1 := &a[1]
 	fmt.Printf("a=%T:%v, len=%d \n", a, a, len(a))
 
 	fmt.Printf("b=%T:%v, len=%d \n", b, b, len(b))
 
 	fmt.Printf("pa=%T: %p \n", pa, pa)
 
-	fmt.Printf("pb=%T: %p \n", pb, pb)
+	fmt.Printf("pa0=%T: %p \n", pa0, pa0)
+
+	fmt.Printf("pa1=%T: %p \n", pa1, pa1)
+
 }
 
 // @func: numbers1
@@ -101,6 +206,15 @@ func main() {
 
 	case '4':
 		numbers4()
+
+	case '5':
+		numbers5()
+
+	case '6':
+		numbers6()
+
+	case '7':
+		numbers7()
 	}
 
 }
--- src/heima_shipin/13_切片.go
+++ src/heima_shipin/13_切片.go
@@ -3,10 +3,60 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
+// @func: slice5
+// @date: 2023年7月20日
+// @brief: string与slice
+// @author: Kewin Li
+func slice5() {
+
+	var str string = "hello world!"
+
+	idx := strings.Index(str, "world")
+	slice1 := str[idx:]
+
+	fmt.Printf("str addr=%p, len=%d\n", &str, len(str))
+	fmt.Printf("slice1 addr=%p, len=%d\n", &slice1, len(slice1))
+
+	slice2 := append([]byte(slice1), '6')
+	fmt.Printf("str addr=%p, len=%d\n", &str, len(str))
+	fmt.Printf("slice2 addr=%p, len=%d\n", &slice2, len(slice2))
+}
+
+// @func: slice4
+// @date: 2023年7月20日
+// @brief: 切片slice的copy操作
+// @author: Kewin Li
+func slice4() {
+
+	// 1. 目的切片的空间足够
+	slice1 := []int{1, 2, 3}
+
+	slice2 := make([]int, 10)
+	fmt.Printf("slice2=%v \n", slice2)
+
+	copy(slice2, slice1)
+	fmt.Printf("slice2=%v \n", slice2)
+
+	fmt.Printf("--------------------\n")
+
+	slice3 := []int{1, 2, 3, 4, 5}
+	fmt.Printf("slice3 addr=%p, len=%d, cap=%d \n", &slice3, len(slice3), cap(slice3))
+	slice4 := make([]int, 3)
+	fmt.Printf("before slice4 addr=%p, len=%d, cap=%d \n", &slice4, len(slice4), cap(slice4))
+
+	copy(slice4, slice3)
+
+	fmt.Printf("slice4=%v \n", slice4)
+
+	fmt.Printf("after slice4 addr=%p, len=%d, cap=%d \n", &slice4, len(slice4), cap(slice4))
+
+}
+
 // @func: slice3
-// @brief: 数组与切片的关系
+// @brief: 数组与切片的关系、append扩容机制
 // @author: Kewin Li
 func slice3() {
 
@@ -14,30 +64,30 @@ func slice3() {
 	arr := []int{1, 2, 3, 4, 5, 6}
 	fmt.Printf("arr= %v \n", arr)
 
-	s1 := arr[:3]
-	s1[0] = 666
+	s1 := arr[:3] // 1 2 3
+	s1[0] = 666   // 666 2 3
 	fmt.Printf("s1=%p, %v\n", &s1, s1)
 	fmt.Printf("arr=%p, %v \n", &arr, arr)
-
+	// arr=xxx 666 2 3 4 5 6
 	fmt.Printf("---------------------- \n")
 
 	// 原数组的修改也会影响切片
 	arr2 := []int{1, 2, 3, 4, 5, 6}
 	fmt.Printf("arr2= %v \n", arr2)
-	s2 := arr2[3:]
-	arr2[4] = 666
-	fmt.Printf("s2=%p, %v \n", &s2, s2)
+	s2 := arr2[3:]                      // 4 5 6
+	arr2[4] = 666                       // 1 2 3 4 666 6
+	fmt.Printf("s2=%p, %v \n", &s2, s2) // s2=0x111 4 666 6
 	fmt.Printf("arr2=%p, %v\n", &arr2, arr2)
 	s2 = append(s2, 777)
 	//s2超过容量上限，会在底层重新开辟新数组copy数据后，再进行追加
 	// 因此，追加的内容没有影响到原数组
-	fmt.Printf("append s2=%p, %v \n", &s2, s2)
-	fmt.Printf("append arr2=%p, %v\n", &arr2, arr2)
+	fmt.Printf("append s2=%p, %v \n", &s2, s2)      // s2=0x222 4 5 6 777
+	fmt.Printf("append arr2=%p, %v\n", &arr2, arr2) // aar=0x000 1 2 3 4 666 6
 
 	fmt.Printf("---------------------- \n")
 
 	// ！！注意: 切片类似一种数组的引用,
-	s3 := arr[1:4]
+	s3 := arr[1:4] // 2 3 4
 	fmt.Printf("s3=%v, len=%d, cap=%d\n", s3, len(s3), cap(s3))
 	s3 = append(s3, 777)
 	fmt.Printf("append s3=%v, len=%d, cap=%d\n", s3, len(s3), cap(s3))
@@ -45,9 +95,9 @@ func slice3() {
 
 	fmt.Printf("---------------------- \n")
 
-	//s3=	2 3 4
+	//s3=2 3 4
 	// low=s3[2]=4, high=arr的长度上限
-	s4 := s3[2:5]
+	s4 := s3[2:5] // 4 777 6
 	fmt.Printf("s4=%v, len=%d, cap=%d\n", s4, len(s4), cap(s4))
 
 }
@@ -108,6 +158,10 @@ func main() {
 		slice2()
 	case '3':
 		slice3()
+	case '4':
+		slice4()
+	case '5':
+		slice5()
 	}
 
 }
