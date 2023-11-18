d167fc65cb1c83d283c46ec242e530dc0202fdc8
Nmoek <707204150@qq.com>
2023-11-18 16:15:19 +0800

    [ADD/CHG] 1. 远程配置中心对接(存在未知BUG); 2. 监听配置变更

--- src/viper/main.go
+++ src/viper/main.go
@@ -4,7 +4,9 @@ package main
 
 import (
 	"fmt"
+	"github.com/fsnotify/fsnotify"
 	"github.com/spf13/viper"
+	_ "github.com/spf13/viper/remote"
 	"os"
 )
 
@@ -29,6 +31,10 @@ func test1() {
 
 }
 
+// @func: test1
+// @date: 2023-11-15 21:16:37
+// @brief: viper用法2
+// @author: Kewin Li
 func test2() {
 	viper.SetConfigType("yaml")
 	viper.SetConfigFile("viper/test_config.yaml")
@@ -43,6 +49,55 @@ func test2() {
 	fmt.Printf("%T %v \n", val, val)
 }
 
+// @func: test3
+// @date: 2023-11-18 10:38:24
+// @brief: 使用etcd远程配置
+// @author: Kewin Li
+func test3() {
+	err := viper.AddRemoteProvider("etcd3", "http://127.0.0.1:2379", "/kitbook")
+	if err != nil {
+		panic(err)
+	}
+
+	//viper.SetConfigName("test_config.yaml")
+	viper.SetConfigType("yaml")
+
+	err = viper.ReadRemoteConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	val := viper.Get("test.key")
+
+	fmt.Printf("%T %v \n", val, val)
+}
+
+// @func: test4
+// @date: 2023-11-18 14:32:08
+// @brief: 监听配置变更
+// @author: Kewin Li
+func test4() {
+	viper.SetConfigFile("viper/test_config.yaml")
+	viper.SetConfigType("yaml")
+
+	//注意: 调用顺序不能改变
+	viper.WatchConfig()
+
+	viper.OnConfigChange(func(in fsnotify.Event) {
+		fmt.Printf("test.key= %v \n", viper.GetString("test.key"))
+	})
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+
+	}
+
+}
+
 func main() {
 	args := os.Args
 
@@ -50,6 +105,10 @@ func main() {
 	case '1':
 		test1()
 	case '2':
-		test1()
+		test2()
+	case '3':
+		test3()
+	case '4':
+		test4()
 	}
 }
